a7d07691804de1ebf9b3d38b99ad86e00afce9c4
Rhylen Nguyen <rhylennguyen@gmail.com>
2025-03-01 21:51:51 -0500

    threshold update

--- src/display.vue
+++ src/display.vue
@@ -5,8 +5,23 @@
 <script>
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime";
-import { ref } from "vue";
-dayjs.extend(relativeTime);
+
+let config = {
+  thresholds: [
+    { l: "s", r: 1 },
+    { l: "m", r: 1 },
+    { l: "mm", r: 59, d: "minute" },
+    { l: "h", r: 1 },
+    { l: "hh", r: 23, d: "hour" },
+    { l: "d", r: 1 },
+    { l: "dd", r: 29, d: "day" },
+    { l: "M", r: 1 },
+    { l: "MM", r: 11, d: "month" },
+    { l: "y", r: 1 },
+    { l: "yy", d: "year" },
+  ],
+};
+dayjs.extend(relativeTime, config);
 export default {
   props: {
     value: {
