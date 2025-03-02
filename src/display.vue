33b02ebe41ddd16eb06012fec013d0ee6f7e9e43
Rhylen Nguyen <rhylennguyen@gmail.com>
2025-03-01 20:54:23 -0500

    junction table test 2

--- src/display.vue
+++ src/display.vue
@@ -1,20 +1,25 @@
 <template>
-	<div>{{ lastEvent }}</div>
+  <div>{{ lastEvent }}</div>
 </template>
 
 <script>
-import dayjs from "dayjs"
-import relativeTime from 'dayjs/plugin/relativeTime'
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { ref } from "vue";
 dayjs.extend(relativeTime);
 export default {
-	props: {
-		value: {
-			type: String,
-			default: null,
-		},
-	},
-	data() {
-		return { lastEvent: this.value.length > 0 ? this.value[0] : ''}
-	}
+  props: {
+    value: {
+      type: String,
+      default: null,
+    },
+  },
+  setup() {
+    const lastEvent = ref("");
+
+    if (this.value.length > 0) {
+      let events = this.value;
+    }
+  },
 };
 </script>
--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import { defineInterface } from '@directus/extensions-sdk'
-import DisplayComponent from './display.vue';
+import { defineInterface } from "@directus/extensions-sdk";
+import DisplayComponent from "./display.vue";
 
 export default defineInterface({
-	id: 'dailydrip-directus-extension-display-last-event',
-	name: 'Display Last Event',
-	icon: 'box',
-	description: 'Displays days since last occuring event',
-	component: DisplayComponent,
-	options: null,
-	types: ['alias'],
-	localTypes: ['m2m'],
-	fields() {
-		return ['events_id']
-	  }
+  id: "dailydrip-directus-extension-display-last-event",
+  name: "Display Last Event",
+  icon: "box",
+  description: "Displays days since last occuring event",
+  component: DisplayComponent,
+  options: null,
+  types: ["alias"],
+  localTypes: ["m2m"],
+  fields() {
+    return ["events_id", "endDate"];
+  },
 });
