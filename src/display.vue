b67a0c1c403ac6523cdc12666e1b274f5ff11e11
Rhylen Nguyen <rhylennguyen@gmail.com>
2025-03-01 19:56:47 -0500

    first commit

---
+++ src/display.vue
@@ -0,0 +1,23 @@
+<template>
+	<div>{{ lastEvent }}</div>
+</template>
+
+<script>
+import dayjs from "dayjs"
+import relativeTime from 'dayjs/plugin/relativeTime'
+dayjs.extend(relativeTime);
+export default {
+	props: {
+		value: {
+			type: String,
+			default: null,
+		},
+	},
+	data() {
+		let endDates = this.value.map((x)=> dayjs(x.endDate));
+		endDates.sort((a, b)=> b.diff(a))
+
+		return {lastEvent: endDates[0].fromNow()}
+	}
+};
+</script>
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { defineInterface } from '@directus/extensions-sdk'
+import DisplayComponent from './display.vue';
+
+export default defineInterface({
+	id: 'dailydrip-directus-extension-display-last-event',
+	name: 'Display Last Event',
+	icon: 'box',
+	description: 'Displays days since last occuring event',
+	component: DisplayComponent,
+	options: null,
+	types: ['alias'],
+	localTypes: ['m2m']
+});
